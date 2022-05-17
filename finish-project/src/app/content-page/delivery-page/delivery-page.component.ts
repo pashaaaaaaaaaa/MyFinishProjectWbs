8aae43281640e8ed51b2006904b7ccc58d42f8a9
pasha <pashaganiev20@gmail.com>
2022-05-17 13:00:23 +0300

    add:component footer

---
+++ finish-project/src/app/content-page/delivery-page/delivery-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-page',
+  templateUrl: './delivery-page.component.html',
+  styleUrls: ['./delivery-page.component.scss']
+})
+export class DeliveryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
