784b24ccf855bd116a8e8884a5be6eb669d7ff4c
pasha <pashaganiev20@gmail.com>
2022-05-17 18:46:10 +0300

    финишная черта

--- finish-project/src/app/app.module.ts
+++ finish-project/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderPageComponent } from './header-page/header-page.component';
 import { ContentPageComponent } from './content-page/content-page.component';
 import { MaterialsPageComponent } from './content-page/materials-page/materials-page.component';
 import { ToolsPageComponent } from './content-page/tools-page/tools-page.component';
+import { LayoutPageComponent } from './content-page/layout-page/layout-page.component';
+import { ApplicationPageComponent } from './content-page/application-page/application-page.component';
+import { OrderPageComponent } from './content-page/order-page/order-page.component';
+import { DeliveryPageComponent } from './content-page/delivery-page/delivery-page.component';
+import { FooterPageComponent } from './content-page/footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { ToolsPageComponent } from './content-page/tools-page/tools-page.compone
     HeaderPageComponent,
     ContentPageComponent,
     MaterialsPageComponent,
-    ToolsPageComponent
+    ToolsPageComponent,
+    LayoutPageComponent,
+    ApplicationPageComponent,
+    OrderPageComponent,
+    DeliveryPageComponent,
+    FooterPageComponent
   ],
   imports: [
     BrowserModule,
